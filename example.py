8f28510ae37016c74459e09e478683121a6a9b4e
Toolkit User <issam.laradji@gmail.com>
2020-08-13 13:35:02 +0000

    added cuda supportt

--- example.py
+++ example.py
@@ -6,22 +6,24 @@ from PIL import Image
 
 if __name__ == "__main__":
     fname = 'base/lungs.jpg'
+    device = 'cpu'
 
     # image_pil = Image.open(fname).resize((10,10))
     image_pil = Image.open(fname)
-    image = torchvision.transforms.ToTensor()(image_pil)[None][:,[0]]
+    image = torchvision.transforms.ToTensor()(image_pil)[None][:,[0]].to(device)
 
     s_time = time.time()
-    et = elastic_transform.ElasticTransform(alpha=1,  sigma=12, random_seed=42)
+    et = elastic_transform.ElasticTransform(alpha=1,  
+                        sigma=12, random_seed=42).to(device)
     image_transformed = et.forward(image)
     e_time = time.time() - s_time
 
     fname_out = 'transformed.jpg'
-    image_pil = torchvision.transforms.ToPILImage()(image_transformed.squeeze())
+    image_pil = torchvision.transforms.ToPILImage()(image_transformed.cpu().squeeze())
     image_pil.save(fname_out)
 
-    org = (image.numpy().squeeze()*255).astype('uint8')
-    out = (image_transformed.detach().numpy().squeeze()*255).astype('uint8')
+    org = (image.cpu().numpy().squeeze()*255).astype('uint8')
+    out = (image_transformed.detach().cpu().numpy().squeeze()*255).astype('uint8')
 
     output = np.concatenate([org, out, ],axis=1)
     Image.fromarray(output).save(fname_out)
